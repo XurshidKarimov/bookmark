0df3757420f4a1f65ee47ed3a2543955fc67f33e
Xurshid <kaxudi34801@gmail.com>
2022-12-06 01:34:09 +0500

    bookmark end

---
+++ js/index.js
@@ -0,0 +1,12 @@
+/** @format */
+
+"use strict";
+
+let tabBTN = documnet.querySelectorAll(".choose");
+let tabs = document.querySelectorAll(".features-content__tabs-block__tab-show");
+
+tabBTN.forEach(function (element) {
+	element.addEventListener("click", () => {
+        
+    });
+});
